2883128e6f7f15c48c283260457bb9e44e277be7
Rozhnov Alexandr <nox73@ya.ru>
2014-02-08 14:58:54 +0400

    Update tank data.

--- js/game/World.js
+++ js/game/World.js
@@ -1,9 +1,11 @@
 define(function(require) {
+  var Tank = require('game/entities/Tank')
   var Entities = require('game/Entities');
+  var _ = require('lodash');
 
   function World() {
     this.terrain = new Entities.Terrain();
-    this.tanks = [];
+    this.tanks = {};
     this.bullets = [];
   }
 
@@ -13,9 +15,30 @@ define(function(require) {
 
   World.prototype.update = function() {}
   World.prototype.draw = function() {}
-  World.prototype.updateState = function() {}
-  World.prototype.setAssetsManager = function() {}
 
+  World.prototype.updateState = function(newState) {
+
+    newState.Tanks.forEach(function(item){
+      var newData = {
+        x: item.Coords.X,
+        y: item.Coords.Y,
+        rotation: item.Coords.Direction
+      }
+
+      tank = this.tanks[item.Id];
+
+      if(_.isUndefined(tank)){
+        tank = new Tank(newData);
+        this.tanks[item.Id] = tank;
+      } else {
+        tank.updateData(newData);
+      }
+
+    }.bind(this))
+
+  }
+
+  World.prototype.setAssetsManager = function() {}
 
   return World;
 })
