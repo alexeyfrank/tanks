c95fb616d63c05ab2dc9055475df3e841b7dfc24
Алексей Франк <alexeyfrank@mbp-aleksej-2.dlink>
2014-02-08 17:13:38 +0400

    assets manager

--- js/lib/AssetsManager.js
+++ js/lib/AssetsManager.js
@@ -1,5 +1,19 @@
 define(function(require) {
-  function AssetsManager() {}
+  function AssetsManager() {
+    this._textures = {};
+  }
+
+  AssetsManager.prototype.loadTexture =function(name, path) {
+    var texture = THREE.ImageUtils.loadTexture(path);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(10, 10);
+
+    this._textures[name] = texture;
+  }
+
+  AssetsManager.prototype.getTexture = function(name) {
+    return  this._textures[name];
+  }
 
   return AssetsManager;
 })
