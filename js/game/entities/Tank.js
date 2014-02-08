d7e9359633f327a261900d7069da3552e774cebb
Алексей Франк <alexeyfrank@mbp-aleksej-2.dlink>
2014-02-09 02:51:12 +0400

    move down tank tower

--- js/game/entities/Tank.js
+++ js/game/entities/Tank.js
@@ -20,6 +20,7 @@ define(function(require) {
     this.towerMesh.scale.z = 5;
 
     this.towerMesh.rotation.y = Math.PI / 2;
+
   }
 
   Tank.prototype.setData = function(data) {
@@ -33,9 +34,9 @@ define(function(require) {
 
     this.towerMesh.position.x = this._data.y;
     this.towerMesh.position.z = this._data.x;
-    this.towerMesh.position.y = 5;
+    this.towerMesh.position.y = 6;
 
-    this.baseMesh.rotation.y = - this._data.rotation * Math.PI/180;
+    this.baseMesh.rotation.y = (- this._data.rotation * Math.PI/180);
     this.towerMesh.rotation.y = this.baseMesh.rotation.y + (- this._data.towerRotation * Math.PI/180);
   }
 
