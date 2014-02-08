ca2808a1cbac19e0bbd822505018a9aeccc39bc7
Алексей Франк <alexeyfrank@mbp-aleksej-2.dlink>
2014-02-08 14:05:18 +0400

    require js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-require(['game'], function(Game) {
-  var Game = require('game');
+require([], function() {
+//  var Game = require('game');
 
-  var game = new Game({
-    id: window.location.hash.slice(1, 10),
-    container: 'game-container',
-    width: 600,
-    height: 600
-  });
-
-  game.preloadAssets(function() {
-    game.create();
-    game.run();
-  });
+//  var game = new Game({
+//    id: window.location.hash.slice(1, 10),
+//    container: 'game-container',
+//    width: 600,
+//    height: 600
+//  });
+//
+//  game.preloadAssets(function() {
+//    game.create();
+//    game.run();
+//  });
 });
